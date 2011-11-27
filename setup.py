82d212e4fc240ce7f18cb4b36776d40cb221ef14
Jonas Baumann <jone@jone.ch>
2011-11-27 15:11:39 +0100

    Update setup.py.
    - Add description
    - Remove Zope3 classifier
    - Change URL to github repository location

--- setup.py
+++ setup.py
@@ -8,27 +8,30 @@ tests_require = [
 
 setup(name='plone.app.lockingbehavior',
       version=version,
-      description="",
+      description="Locking integration for dexterity content objects.",
       long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
-        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='dexterity locking behavior plone',
       author='Plone Foundation',
       author_email='mailto:dexterity-development@googlegroups.com',
-      url='http://svn.plone.org/svn/plone/plone.app.lockingbehavior/',
+      url='https://github.com/plone/plone.app.lockingbehavior/',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         'plone.behavior',
@@ -37,6 +40,7 @@ setup(name='plone.app.lockingbehavior',
         ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
